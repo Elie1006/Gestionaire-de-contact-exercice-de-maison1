6503b10cc8fa074e3c5ae54d3f50c14842119c8a
Bechehin <eliebechehin77@gmail.com>
2026-04-24 19:08:13 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+        // création du gestionnaire de contact
+        ContactsManager monGestionnaire = new ContactsManager();
+
+        // creation du premier contact
+        Contact contact1 = new Contact();
+        contact1.nom = "Yapo Jean Elie";
+        contact1.numeroTelephone = "07.07.94.15.95";
+        monGestionnaire.addContact(contact1);
+
+        // creation du deuxième contact
+        Contact contact2 = new Contact();
+        contact2.nom = "Yankey Ange";
+        contact2.numeroTelephone = "01.51.90.38.92";
+        monGestionnaire.addContact(contact2);
+
+        // creation du troisième contact
+        Contact contact3 = new Contact();
+        contact3.nom = "Zouzou Ange Patrick ";
+        contact3.numeroTelephone = "05.86.17.26.77";
+        monGestionnaire.addContact(contact3);
+
+        // creation du quatrième contact
+        Contact contact4 = new Contact();
+        contact4.nom = "Ouattara Fanta";
+        contact4.numeroTelephone = "05.85.56.44.89";
+        monGestionnaire.addContact(contact4);
+
+        // Je crée le cinquième contact
+        Contact contact5 = new Contact();
+        contact5.nom = "Bamba Ousmane";
+        contact5.numeroTelephone = "01.52.86.18.98";
+        monGestionnaire.addContact(contact5);
+
+        // recherche d'un contact et affichage du numéro
+        Contact contactTrouve = monGestionnaire.searchContact("Zouzou Ange Patrick");
+
+        // Verification du contact trouvé
+        if (contactTrouve != null) {
+            System.out.println("Contact trouvé : " + contactTrouve.nom);
+            System.out.println("Son numéro est : " + contactTrouve.numeroTelephone);
+        } else {
+            System.out.println("contact introuvable");
+        }
+    }
+}
